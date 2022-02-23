8c4d7be897544863c4db6e2cf2f06705406123b5
thewalczi <walczak.kam@gmail.com>
2022-02-23 21:33:57 +0100

    Fix rules in eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React from 'react';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      Where the air
-    </div>
-  );
+	return <div className="App">Where the air </div>
 }
-
-export default App;
+export default App
