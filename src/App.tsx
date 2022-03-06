40641cce18f98bdd8ddccc63780614fab7f4d3ab
thewalczi <walczak.kam@gmail.com>
2022-03-06 16:59:15 +0100

    Add topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
 import { MapWrapper } from "./components/map/MapWrapper"
+import { TopBar } from "./components/topbar/TopBar"
 import { LayoutPortal } from "./layout/LayoutPortal"
 
 function App() {
 	return (
 		<LayoutPortal>
+			<TopBar />
 			<MapWrapper />
 		</LayoutPortal>
 	)
---
+++ src/components/topbar/TopBar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react"
+
+import "./TopBar.scss"
+
+export const TopBar: FC = () => {
+	return (
+		<div className="topbar">
+			<div className="topbar__logo">Where The Air</div>
+		</div>
+	)
+}
