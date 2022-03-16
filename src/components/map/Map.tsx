898a06dc821afddde567738369ddcac1aff4fe7c
thewalczi <walczak.kam@gmail.com>
2022-03-16 20:53:27 +0100

    Create button to get center coordinates

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,27 +1,45 @@
 import React, { FC } from "react"
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet"
 
 interface MapProps {
 	coordinates: [number, number]
 }
 
+const Button = () => {
+	const map = useMap()
+	const handleClick = () => {
+		alert(map.getCenter())
+	}
+	return (
+		<button
+			onClick={handleClick}
+			style={{ position: "relative", zIndex: 1000 }}
+		>
+			Get Center
+		</button>
+	)
+}
+
 export const Map: FC<MapProps> = ({ coordinates }: MapProps) => {
 	return (
-		<MapContainer
-			key={JSON.stringify(coordinates)}
-			center={coordinates}
-			zoom={15}
-			style={{ height: "100%" }}
-		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>
-					A pretty CSS3 popup. <br /> Easily customizable.
-				</Popup>
-			</Marker>
-		</MapContainer>
+		<>
+			<MapContainer
+				key={JSON.stringify(coordinates)}
+				center={coordinates}
+				zoom={15}
+				style={{ height: "calc(100% - 68px)" }}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				{/* <Marker position={coordinates}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker> */}
+				<Button />
+			</MapContainer>
+		</>
 	)
 }
